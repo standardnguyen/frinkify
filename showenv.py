6b0db8b3ddef5dd5e846e938fb1cebf0c476863d
Standard <standardvyang@gmail.com>
2025-03-15 12:05:13 -0600

    env stuff

---
+++ showenv.py
@@ -0,0 +1,4 @@
+source_directory = "./Veep Raw"  # Replace with the path to your directory containing seasons
+destination_directory = "./Veep"
+subtitles_directory = "./Veep Subtitles"
+show_name = "Veep"
